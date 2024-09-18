d53cfc7220b5da439cbe22aa38b8e8fae24f5dc5
Alina <alinasackovskaa425@gmail.com>
2024-09-18 12:33:03 +0300

    Vue.js lab1

---
+++ lab1/Sachkovska/src/components/HelloWorld.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String,
+})
+
+const count = ref(0)
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+
+  <div class="card">
+    <h3>
+      Program Ingeneering, 8.1213, 2nd year, Sachkovska Alina Ihorivna
+    </h3>
+  </div>
+
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
